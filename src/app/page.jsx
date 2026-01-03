f7e644484d0227ab47fe7d989c996d3fdf358b17
abhinav <abhinavisgreat18@gmail.com>
2026-01-03 15:53:53 +0530

    minor update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
+import Container from "@/components/general/container"
 import AuthRedirect from "@/components/route/authRedirect"
 import Hero from "@/components/section/hero"
 
 export default async function Home() {
   return (
     <AuthRedirect>
-      <Hero />
+      <Container>{/* <Hero /> */}</Container>
     </AuthRedirect>
   )
 }
