6460dc16ecea3533c57c739279a324e8ebefcb8e
abhinav <abhinavisgreat18@gmail.com>
2026-01-07 17:12:58 +0530

    updated for deployment

--- src/app/(private)/my-blog/page.jsx
+++ src/app/(private)/my-blog/page.jsx
@@ -1,4 +1,4 @@
-import BlogCard from "@/components/blog/blog-card"
+import BlogCard2 from "@/components/blog/blog-card2"
 import { getMyBlogs } from "@/helpers/blog-server/my-blog"
 
 const MyBlogs = async () => {
@@ -7,7 +7,7 @@ const MyBlogs = async () => {
     <main>
       <div className='w-2/3 grid grid-cols-2 auto-rows-max gap-x-10 gap-y-8 mt-4'>
         {blogs.map((i, index) => {
-          return <BlogCard key={index} data={i} />
+          return <BlogCard2 key={index} data={i} />
         })}
       </div>
     </main>
